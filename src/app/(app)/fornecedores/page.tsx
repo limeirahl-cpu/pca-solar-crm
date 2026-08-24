0c9bd5a9aa050781e15aee9ba2d26d276b8c8a56
limeirahl-cpu <limeirahl@gmail.com>
2026-08-24 02:10:11 -0300

    Delete src directory

--- src/app/(app)/fornecedores/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/lib/supabase/server";
-import { SuppliersManager } from "@/components/estoque/SuppliersManager";
-
-export default async function FornecedoresPage() {
-  const supabase = await createClient();
-  const { data: suppliers } = await supabase.from("suppliers").select("*").order("nome");
-
-  return <SuppliersManager initialSuppliers={suppliers ?? []} />;
-}
